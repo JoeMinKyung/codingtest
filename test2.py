85462f931076d1169aef80ec09371c853ac4c5f6
JoeMinKyung <mk981015@naver.com>
2024-05-12 23:30:27 +0900

    test

--- test2.py
+++ test2.py
@@ -16,15 +16,18 @@ def numIslands(grid):
         
         while q:
             cur_x,cur_y=q.popleft()
+            
             for i in range(4):
                 next_x=cur_x+dx[i]
                 next_y=cur_y+dy[i]
                 
                 if 0<=next_x<row and 0<=next_y<col:
+                   
                     if visited[next_x][next_y]==False and grid[next_x][next_y]=="1":
                         q.append((next_x,next_y))
                         visited[next_x][next_y]=True
         return visited
+    
     for i in range(row):
         for j in range(col):
             if grid[i][j]=="1" and not visited[i][j]:
